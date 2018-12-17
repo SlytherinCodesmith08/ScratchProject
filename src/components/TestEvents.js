1712d3fc1174e3d61bab3ed0c146ae58572967ae
Leury Rodriguez <leuryr@yahoo.com>
2018-12-17 15:46:19 -0500

    Update file structure, events dummy data

---
+++ src/components/TestEvents.js
@@ -0,0 +1,79 @@
+export const events = [
+  {
+    id: 2321,
+    firstname: 'Brian',
+    lastname: 'Hon',
+    title: 'title1',
+    description: 'Hello everyone. I will be preparing an excquisite steak dinner with wine. Come on over and taste what the chef is cooking',
+    time: '2:34 PM',
+    lat: 40.72227, lon: -74.000723,
+    email: 'sample@gmail.com',
+    capacity: 4
+  },
+  {
+    id: 2302,
+    firstName: 'Eric',
+    lastName: 'Choy',
+    title: 'title2',
+    description: 'Hello everyone. I am making a big pizza. Come and get it!',
+    time: '3:10 PM',
+    lat: 40.722033, lon: -73.997242,
+    email: 'sample@gmail.com',
+    capacity: 2
+  },
+  {
+    id: 2032,
+    firstName: 'Altai',
+    lastName: 'Chen',
+    title: 'title3',
+    description: 'White cupcakes with a chocolate egg in the center, thick, colorful icing, and topped with a chocolate mini egg!!! Great Easter treat!!',
+    time: '7:34 PM',
+    lat: 40.7227, lon: -73.998857,
+    email: 'sample@gmail.com',
+    capacity: 4
+  },
+  {
+    id: 2423,
+    firstName: 'Jason',
+    lastName: 'Luble',
+    title: 'title4',
+    description: 'Pasta',
+    time: '7:34 PM',
+    lat: 40.642309, lon: -73.946436,
+    email: 'sample@gmail.com',
+    capacity: 10
+  },
+  {
+    id: 3231,
+    firstName: 'Peter',
+    lastName: 'Griffen',
+    title: 'title4',
+    description: 'Pasta',
+    time: '7:34 PM',
+    lat: 40.760798, lon: -73.989396,
+    email: 'sample@gmail.com',
+    capacity: 3
+  },
+  {
+    id: 3523,
+    firstName: 'Noah',
+    lastName: 'Bat',
+    title: 'title4',
+    description: 'Pasta',
+    time: '7:34 PM',
+    lat: 40.72503, lon: -74.004741,
+    email: 'sample@gmail.com',
+    capacity: 1
+  },
+  {
+    id: 4523,
+    firstName: 'Cat',
+    lastName: 'Bat',
+    title: 'title4',
+    description: 'Pasta',
+    time: '7:34 PM',
+    lat: 40.73224, lon: -73.977265,
+    email: 'sample@gmail.com',
+    capacity: 6
+  }
+];
